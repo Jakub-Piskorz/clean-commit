49f8638499be32f0107a44e970ef38fc3926b844
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-09-07 18:04:19 +0200

    PurgeCSS exception for datepicker

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,70 +1,70 @@
-const path = require('path')
-const tailwind = require('tailwindcss')
+const path = require("path");
+const tailwind = require("tailwindcss");
 
 module.exports = {
   siteMetadata: {
-    baseTitle: 'Hotely',
-    separator: '|',
+    baseTitle: "Hotely",
+    separator: "|",
     siteUrl: `localhost:8000`,
-    title: 'Recruitment task',
-    image: '',
-    themeColor: '#fff',
-    keyword: 'online Hotel finder',
-    description: 'Blazing fast Hotel reservations',
+    title: "Recruitment task",
+    image: "",
+    themeColor: "#fff",
+    keyword: "online Hotel finder",
+    description: "Blazing fast Hotel reservations"
   },
   plugins: [
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
             resolve: `gatsby-remark-relative-images`,
             options: {
-              name: 'uploads',
-            },
+              name: "uploads"
+            }
           },
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
-            },
-          },
-        ],
-      },
+              maxWidth: 590
+            }
+          }
+        ]
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
+        name: "uploads"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [
           tailwind,
-          require('./tailwind.config.js'), // Optional: Load custom Tailwind CSS configuration
-        ],
-      },
+          require("./tailwind.config.js") // Optional: Load custom Tailwind CSS configuration
+        ]
+      }
     },
     {
       resolve: `gatsby-plugin-purgecss`,
@@ -72,37 +72,38 @@ module.exports = {
         printRejected: true,
         develop: false, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-      },
+        ignore: ["react-datepicker/dist/react-datepicker.css"]
+      }
     },
     {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         googleAnalytics: {
-          trackingId: '',
-          anonymize: true,
+          trackingId: "",
+          anonymize: true
         },
         facebookPixel: {
-          pixelId: '',
-        },
-      },
+          pixelId: ""
+        }
+      }
     },
     {
-      resolve: 'gatsby-plugin-root-import',
+      resolve: "gatsby-plugin-root-import",
       options: {
-        '@': path.join(__dirname, 'src'),
-        '~': path.join(__dirname),
-        styles: path.join(__dirname, 'src/styles'),
-        img: path.join(__dirname, 'static/img'),
-      },
+        "@": path.join(__dirname, "src"),
+        "~": path.join(__dirname),
+        styles: path.join(__dirname, "src/styles"),
+        img: path.join(__dirname, "static/img")
+      }
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
         manualInit: true,
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
     `gatsby-plugin-advanced-sitemap`,
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
+};
